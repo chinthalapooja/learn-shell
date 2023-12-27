e1af671313b6690e7b4c0d9e413f3b78622a611c
chinthalapooja <pujachinthala@gmail.com>
2023-12-27 20:30:54 +0530

    shell scripting

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,15 @@
 echo 'helloworld'
 echo -e "\e[32m green colour \e[0m"
 echo 'name'
+
+# Color Syntax: echo -e "\e[COLm MESSAGE \e[0m"
+# echo -e , -e is to allow colors
+# Double quotes are mandatory, Single quotes can be used but not preferred.
+# \e[COLm -> COL is a color code we need provide
+  # RED       - 31
+  # GREEN     - 32
+  # YELLOW    - 33
+  # BLUE      - 34
+  # MAGENTA   - 35
+  # CYAN      - 36
+# \e[0m -> THis is to disable the enabled color
